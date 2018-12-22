0c514e444a2976830fe91aa50f0f386dea942429
Achala Dissanayake <aidissanayake@gmail.com>
2018-12-23 00:34:00 +0530

    padding and key importing functions  added

--- src/RSA.php
+++ src/RSA.php
@@ -1,36 +1,37 @@
 <?php namespace kare\kare_enc;
 
 /**
-*  A sample class
-*
-*  Use this section to define what this class is doing, the PHPDocumentator will use this
-*  to automatically generate an API documentation using this information.
-*
-*  @author Achala
+*  @author Achala Dissanayake
 */
+
 final class RSA{
 
-   /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
-   private $enc_algo = 'RSA';
- 
-  /**
-  * Sample method 
-  *
-  * Always create a corresponding docblock for each method, describing what it is for,
-  * this helps the phpdocumentator to properly generator the documentation
-  *
-  * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
-  *
-  * @return string
-  */
+    public static function importPrivateKeyString($keyString){
+        return openssl_pkey_get_private($keyString);
+    }
 
-   public static function publicKeyEncrypt($message, $publicKey){
-        openssl_public_encrypt($message, $encrypted, $publicKey);
+    public static function importPrivateKeyFile($filePath){
+        $keyString = file_get_contents($filePath);
+        return openssl_pkey_get_private($keyString);
+    }
+
+    public static function importPublicKeyString($keyString){
+        return openssl_pkey_get_public($keyString);
+    }
+
+    public static function importPublicKeyFile($filePath){
+        $keyString = file_get_contents($filePath);
+        return openssl_pkey_get_public($keyString);
+    }
+    
+    public static function publicKeyEncrypt($message, $publicKey){
+        openssl_public_encrypt($message, $encrypted, $publicKey, $padding = OPENSSL_PKCS1_OAEP_PADDING);
         return $encrypted;
-   }
+    }
 
-   public static function privateKeyDecrypt($encryptedMessage, $privateKey){
-        openssl_private_decrypt($encryptedMessage, $decrypted, $privateKey);
+    public static function privateKeyDecrypt($encryptedMessage, $privateKey){
+        openssl_private_decrypt($encryptedMessage, $decrypted, $privateKey, $padding = OPENSSL_PKCS1_OAEP_PADDING);
         return $decrypted;
-   }
+    }
+
 }
--- tests/RSATest.php
+++ tests/RSATest.php
@@ -1,21 +1,12 @@
 <?php 
 
 /**
-*  Corresponding Class to test YourClass class
-*
-*  For each class in your library, there should be a corresponding Unit-Test for it
-*  Unit-Tests should be as much as possible independent from other test going on.
-*
-*  @author yourname
+*  @author Achala Dissanayake
 */
 class RSATest extends PHPUnit_Framework_TestCase{
 	
   /**
-  * Just check if the YourClass has no syntax error 
-  *
-  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
-  * any typo before you even use this library in a real project.
-  *
+  * Check if the class has no syntax error 
   */
 
   public function testIsThereAnySyntaxError(){
@@ -24,28 +15,53 @@ class RSATest extends PHPUnit_Framework_TestCase{
     unset($var);
   }
   
-  /**
-  * Just check if the YourClass has no syntax error 
-  *
-  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
-  * any typo before you even use this library in a real project.
-  *
-  */
-
   public function testPublicKeyEncrypt(){
     $var = new kare\kare_enc\RSA;
-    $pubkey = openssl_pkey_get_public(file_get_contents('sample_public.key'));
-    $this->assertTrue(strlen($var->publicKeyEncrypt("abcde", $pubkey)) > 0);
+    $pubKey = openssl_pkey_get_public(file_get_contents('sample_public.key'));
+    $this->assertTrue(strlen($var->publicKeyEncrypt("abcde", $pubKey)) > 0);
     unset($var);
   }
   
   public function testPrivateKeyDecrypt(){
     $var = new kare\kare_enc\RSA;
-    $pubkey = openssl_pkey_get_public(file_get_contents('sample_public.key'));
-    $pvtkey = openssl_pkey_get_private(file_get_contents('sample_private.key'));
-    $enc = $var->publicKeyEncrypt("abcde", $pubkey);
-    $dec = $var->privateKeyDecrypt($enc, $pvtkey);
-    $this->assertTrue($var->privateKeyDecrypt($enc, $pvtkey) == 'abcde');
+    $pubKey = openssl_pkey_get_public(file_get_contents('sample_public.key'));
+    $pvtKey = openssl_pkey_get_private(file_get_contents('sample_private.key'));
+    $enc = $var->publicKeyEncrypt("abcde", $pubKey);
+    $dec = $var->privateKeyDecrypt($enc, $pvtKey);
+    $this->assertTrue($dec == 'abcde');
     unset($var);
   }
+
+  public function testImportPrivateKeyString(){
+    $var = new kare\kare_enc\RSA;
+    $pvtKeyString = "-----BEGIN RSA PRIVATE KEY-----\nMIIEoQIBAAKCAQBGIdM1Sg1AEV7fR2auPildd+YQKkhzm0AbFPesm/ZaB7cSnfCb\nb9KhxSH63JVsJvK9mAT4r8ctmbKHTLkwLx6mw652g1Px7qDHAvQghkoXlsRjE2Cv\nuiIx1hAdp9QezkFaNB5yutHhOs6PJGVOnG/0zaQJ3516vtFV6PyJe/KWTvVs5Feq\naJMWi4UjOgm71q7V6/rY+cP64xplF4lZPN6v2RtsM/wxz2pr+rLvIMbNoX2y8RWR\nYRM3NWlE9dCrfh4aul6tGPdATXutgaYXHDRTGL7FxPrPI63RgsNSqxyXVq1J8ccS\ntnuB9QQ/7YbIArMf+z6FLEm32kIokn1yBQ5RAgMBAAECggEAPwKxAtXvpbp1pOI/\n7RlIjQPmnmt/AO9h33MtO9Y8tpLr9zwUK3OCqXm2l++MCMnNmm51OQKlT+Eht9JF\nKHeNcIOrwisoRtwBdAzBTl+ZPixlcia83eW8R05u2FYrjSn/KY5eNPKJE3WElLM8\nmi6PoEEKuxxAH0JGTouwKc5FHKoPlTiaSAOE+F6qkaKjm09uYoXt0pVZg/g9JDQU\nAJqJsHa9tqwzbv/vU7g7m8H5y6hCxTK38JQc+541Tj5ka5wmCYL3YuuqqrM3WnzS\nV5XaFYgKex/Gldu+lH/4wayztPqxEog12Arq4OclUix8Erm0Ry6S35Z9uJNzvd7R\n71JO4QKBgQCJys0NK7/0/K7bGJfX/s4n9OlSi6bG2XL+MTZ9gt/UCjOhHjbgNTqv\ngLMZKOgKwRKxbsQRi7FEK4UE2wwX5CYZWitmQ5Cl9RcX7Fw9PBDxLz75BfQXXERI\nB23etTQxUwl9mqeQwtCc2sBCAd1IwVdV6NzlD9A2vXfGzNYSfoA4XQKBgQCCS+Au\nlrPCwJ+Dr1yifcddfT9Hqxb9itFqQkpqae4UMLElahesVUJL98JqtNUst2UuGaG2\nDdNL4tVD5lqBofo27z+IzJOkBRODKdQqTuQyCfkGBTHygrtGc0PIGIp8ewu1wwXU\nilhFQlcABqGi4w3elLqjuYrvuPJw2T4KUbJ+hQKBgG2quwiAiYaylXhLWo7OfcXP\nZuQLwfEBoEQvZu79+qcId48EPSi1NL/57pFXvVbR087kGajdRXi8KmJy9G0PaENF\nQMVjgPyU1Ix10fPqmpFayQPpeRwekznAr/FQxvl63VLahALzCpXMhLgEQ9pkBt6Z\nNzYYH99xbKMM5FRT2jkBAoGAAo+LMF812TX/7I6du3PNX0D+5WGvafw4cWYsIDtE\nupDaamnTXUevrd6Iq6AyMFbKXkWSsAlFhdBHcLYuQS31xO6eyQl8PVT8NS+1Q7+Z\nLIKVqlCf0qxPEetiAaS51Ah3CnNyweKWKYZMP2vh8VmVBDLYGC+lU2DHIwCh/4Hr\niCECgYAcDUeDWZeoAROW9TpHSx80EXE5Olfmd9kolBOLIukEeYdV89EyW7uOfrlf\n0iMvMD7wDpxH8BK83nHU5l4/PAm+znoo2PWd7Hvr+90f4ST0yDfaVjSkjY7+cfhr\nsCW6jKCgS5h2VI0QPn4zwWaGACRhkT1RfRXJXsaDgUknz+2XZA==\n-----END RSA PRIVATE KEY-----";
+    $pvtKey = $var->importPrivateKeyString($pvtKeyString);
+    $pubKey = openssl_pkey_get_public(file_get_contents('sample_public.key'));
+    $enc = $var->publicKeyEncrypt("abcde", $pubKey);
+    $dec = $var->privateKeyDecrypt($enc, $pvtKey);
+    $this->assertTrue($dec == 'abcde');
+    unset($var);
+  }
+
+  public function testImportKeyFile(){
+    $var = new kare\kare_enc\RSA;
+    $pvtKey = $var->importPrivateKeyFile('sample_private.key');
+    $pubKey = $var->importPublicKeyFile('sample_public.key');
+    $enc = $var->publicKeyEncrypt("abcde", $pubKey);
+    $dec = $var->privateKeyDecrypt($enc, $pvtKey);
+    $this->assertTrue($dec == 'abcde');
+    unset($var);
+  }
+
+  public function testImportPublicKeyString(){
+    $var = new kare\kare_enc\RSA;
+    $pvtKey = openssl_pkey_get_private(file_get_contents('sample_private.key'));
+    $pubKeyString = "-----BEGIN PUBLIC KEY-----\nMIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQBGIdM1Sg1AEV7fR2auPild\nd+YQKkhzm0AbFPesm/ZaB7cSnfCbb9KhxSH63JVsJvK9mAT4r8ctmbKHTLkwLx6m\nw652g1Px7qDHAvQghkoXlsRjE2CvuiIx1hAdp9QezkFaNB5yutHhOs6PJGVOnG/0\nzaQJ3516vtFV6PyJe/KWTvVs5FeqaJMWi4UjOgm71q7V6/rY+cP64xplF4lZPN6v\n2RtsM/wxz2pr+rLvIMbNoX2y8RWRYRM3NWlE9dCrfh4aul6tGPdATXutgaYXHDRT\nGL7FxPrPI63RgsNSqxyXVq1J8ccStnuB9QQ/7YbIArMf+z6FLEm32kIokn1yBQ5R\nAgMBAAE=\n-----END PUBLIC KEY-----";
+    $pubKey = $var->importPublicKeyString($pubKeyString);
+    $enc = $var->publicKeyEncrypt("abcde", $pubKey);
+    $dec = $var->privateKeyDecrypt($enc, $pvtKey);
+    $this->assertTrue($dec == 'abcde');
+    unset($var);
+  }
+  
 }
